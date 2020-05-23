0384cae07a082579a7e9930735059b459bfef182
Darshan Mistry <51168163+Darshanmistry1995@users.noreply.github.com>
2020-05-22 23:43:41 -0400

    Update ListITR.cpp

--- Homework/ListITR.cpp
+++ Homework/ListITR.cpp
@@ -15,7 +15,7 @@ int main()
 	
 	// Displaying vector elements using begin() and end() 
 
-	cout << "The vector elements are.. : "; 
+	cout << "The vector elements are.: "; 
 	for (ptr = arry.begin(); ptr < arry.end(); ptr++) 
 		cout << *ptr << " "; 
 	
